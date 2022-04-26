b228370a17dd015d3f45f406cc46f7a14dd8b4fe
Gee <glynester@hotmail.com>
2022-04-26 18:45:30 +0100

    Routes set up for components. Semantic-ui styling added. Header set up.

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-// import { HashRouter, Route, Link } from 'react-router-dom';
-// import { MemoryRouter, Route, Link } from 'react-router-dom';
-// Use Link in place of anchor tags - anchor tags are bad.
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
 
-const PageOne=()=>{
-  return (
-  <div>
-    PageOne<br/>
-    {/* BAD! Don't use anchor tags with react router */}
-    {/* <a href='/pagetwo'>Page Two</a> */}
-    <Link to='/pagetwo'>Page Two</Link> 
-  </div>)
-}
-const PageTwo=()=>{
-  return (
-  <div>
-    PageTwo
-    <button>Click Me</button><br/>
-    {/* BAD! Don't use anchor tags with react router */}
-    {/* <a href='/'>Page One</a> */}
-    <Link to='/'>Page One</Link>
-  </div>)
-}
 
 const App=()=>{
   return (
-    // BrowserRouter is itself an app component.
-    // Show the required component  if the path matches
-    // Different routes can be matched by the same URL.
-    // We can very easily have multiple root components that match a given URL and all show themselves to the user.
-    // exact = {true} is the same as exact
-    <div>   
+    <div className='ui container'>  
+      {/* <h1>HEADER</h1>  */}
+      {/* <Header /> This needs to be moved inside the router */}
       <BrowserRouter>
-      {/* <HashRouter> */}
-      {/* <MemoryRouter> */}
-        {/* <Route path='/' exact component={PageOne} />  */}
-        <Route path='/' exact  component={PageOne} />
-        <Route path='/pagetwo' component={PageTwo} />
-      {/* </MemoryRouter> */}
-      {/* </HashRouter> */}
+        <Header />
+        <Route path='/' exact component={StreamList}/>
+        <Route path='/streams/new' component={StreamCreate}/>
+        <Route path='/streams/edit' component={StreamEdit}/>
+        <Route path='/streams/delete' component={StreamDelete}/>
+        <Route path='/streams/show' component={StreamShow}/>
       </BrowserRouter>
     </div> 
   )
