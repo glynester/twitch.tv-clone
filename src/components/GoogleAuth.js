cc12ec5151cbf7f1707be83523ae56f8fa8eb8fd
Gee <glynester@hotmail.com>
2022-04-27 21:21:38 +0100

    Renders and updates authentication status using manual changes to login status though console 'gapi.auth2.getAuthInstance().signIn();'

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state={ isSignedIn: null }// Initialise state
   componentDidMount(){  // When component is loaded to screen, load up client portion of library
     window.gapi.load('client:auth2',()=>{
       // clientId set up here: https://console.cloud.google.com/apis/credentials?project=twitch-tv-clone
       window.gapi.client.init({
-        clientId: '242034589938-fgvj2j79h5u0eoae5kfsf25n5he3jkkc.apps.googleusercontent.com'
-      }); // Callback only loads after 'client:auth2' library has been successfully loaded into gapi.
+        clientId: '242034589938-fgvj2j79h5u0eoae5kfsf25n5he3jkkc.apps.googleusercontent.com',
+        scope: 'email'
+      }) // Callback only loads after 'client:auth2' library has been successfully loaded into gapi.
+      // client.init returns a promise.
+      .then(()=>{
+        this.auth=window.gapi.auth2.getAuthInstance();  // auth object
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()} ) // Update component level state to get component to re-render.
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      })
     }); // Available on window scope inside browser
-    
-
   }
+
+  onAuthChange=()=>{
+    this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+  }
+
+  renderAuthButton(){ // helper function
+    if (this.state.isSignedIn===null){
+      return <div>Not sure if signed in</div>
+    } else if (this.state.isSignedIn){ 
+      return <div>Signed in</div>
+    } else return <div>Not Signed in</div>
+  }
+
   render(){
     return (
-      <div>Google Auth</div>
+      <div>{this.renderAuthButton()}</div>
     )
   }
 }
