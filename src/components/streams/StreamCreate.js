13001c87ab575ea5a6f65758b9b4686ec0fd1026
Gee <glynester@hotmail.com>
2022-05-03 21:07:15 +0100

    Wiring up redux-form. Setting up StreamCreate form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';  // Field is a component and reduxForm is a function.
 
-const StreamCreate=()=>{
-  return (
-    <div>StreamCreate</div>
-  )
+class StreamCreate extends React.Component{
+  // Helper method:
+  // renderInput(formProps){
+    // return <div>I'm an input</div>
+    renderInput({ input }){ // destructured from formProps
+    // console.log("formProps",formProps);
+    // return <input onChange={formProps.input.onChange} value={formProps.input.value}/>
+    return <input {...input } />  // Alternative syntax to get other properties inside 'input' object (besides just 'value' and 'onchange')  that redux form cares about.
+
+  }
+
+  render() {
+    console.log("Props",this.props);  // Many props available now from reduxForm helper
+    return (
+      <form>
+        <Field name="Title" component={this.renderInput} />
+        <Field name="Description" component={this.renderInput} />
+      </form>
+    )
+  }
 }
 
-export default StreamCreate;
+// const StreamCreate=()=>{
+//   return (
+//     <div>StreamCreate</div>
+//   )
+// }
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate); // 'reduxForm' returns a function and we immediately call that function with 'StreamCreate'
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';  // renaming reducer to make it less ambiguous.
 import authReducer from './authReducer';
 
 export default combineReducers({
   // replaceMe:()=>"TEST VALUE"    // Need to have one reducer at least.
-  auth: authReducer
+  // Key has to be "form".
+  auth: authReducer,
+  form : formReducer
 })
