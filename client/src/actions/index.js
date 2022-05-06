7331aad1f99267b3c1bf7b17626defc7e6255aeb
Gee <glynester@hotmail.com>
2022-05-06 21:05:46 +0100

    Action creators done for all RESTful routes.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
+import { formValues } from 'redux-form';
 import streams from '../apis/streams';  // instance of axios
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from "./types";
 
 
 export const signIn=(userId)=>{
@@ -20,8 +21,33 @@ export const signOut=()=>{
 export const createStream=(formValues)=>{
   // asynchronous action creator - any time we make an asynchronous action creator, we are making use of redux thunk.
   return async (dispatch)=>{
-    streams.post('/streams', formValues); // post request with axios
+    const response=await streams.post('/streams', formValues); // post request with axios
     // this is going to create a stream because we are following RESTful conventions.
+    dispatch({ type: CREATE_STREAM, payload: response.data })
   }
+}
+
+export const fetchStreams=()=>{
+  return async (dispatch)=>{
+    const response= await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  }
+}
+
+export const fetchStream=(id)=>async dispatch=>{
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+}
+
+// To edit a stream, we need to provide both the ID and the update that we're trying to make to that stream as arguments to the action creator.
+export const editStream=(id, formValues)=>async dispatch=>{
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+}
+
+// No response needed for 'delete' with axios. Payload is id of deleted stream.
+export const deleteStream=(id)=>async dispatch=>{
+  await streams.delete(`/stream/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
 }
 
