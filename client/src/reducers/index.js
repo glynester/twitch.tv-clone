069de5b69780e3189273c82a88486f68ad951724
Gee <glynester@hotmail.com>
2022-05-10 12:50:53 +0100

    Reducers for all routes. lodash installed.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';  // renaming reducer to make it less ambiguous.
 import authReducer from './authReducer';
+import streamReducer from'./streamReducer';
 
 export default combineReducers({
   // replaceMe:()=>"TEST VALUE"    // Need to have one reducer at least.
   // Key has to be "form".
   auth: authReducer,
-  form : formReducer
+  form : formReducer,
+  streams: streamReducer
 })
