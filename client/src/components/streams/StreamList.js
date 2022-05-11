3f41e66c8ff8c2f335a53b5a6b0e17bd8b731351
Gee <glynester@hotmail.com>
2022-05-11 13:08:22 +0100

    Wired up action creator call to stream list component. All streams rendered to browser.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList=()=>{
-  return (
-    <div>StreamList</div>
-  )
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+
+  renderList(){
+    return this.props.streams.map(stream=>{
+      return (
+        <div className='item' key={stream.id}>
+          <i className='large middle aligned icon camera'></i>
+          <div className='content'>
+            {stream.title}
+            <div className='description'>
+            {stream.description}
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render(){
+    console.log("Streams=>",this.props.streams)
+    return (
+    <div>
+      <h2>Streams</h2>
+      <div className='ui celled list'>
+        {this.renderList()}
+      </div>
+    </div>
+    )
+  }
 }
 
-export default StreamList;
+const mapStateToProps=(state)=>{
+  // Turn streams object from store back into an array
+  return { streams: Object.values(state.streams)}
+}
+// const StreamList=()=>{
+//   return (
+//     <div>StreamList</div>
+//   )
+// }
+
+// export default StreamList;
+// export default connect(null, { fetchStreams })(StreamList);  // Interim stage before mapStateToProps
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
