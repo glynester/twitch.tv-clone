5227e0bbaab0c4d00a72dc759fe9ff2ad2a3e85e
Gee <glynester@hotmail.com>
2022-05-19 21:19:23 +0100

    Modal made reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';     // Used here to navigate to another url (way to make modal window disappear) when user clicks on background modal screen.
 
 const Modal=props=>{
   // Don't return jsx like for normal component
   return ReactDOM.createPortal(   // 2 arguments
-    <div onClick={()=>history.push('/')} className="ui dimmer modals visible active">
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       {/* stopPropagation() - prevents a click on any of the elements inside of here, from bubbling up to this div.*/}
       <div onClick={e=>e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure you want to delete this stream?</div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
         <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
+          {props.actions}
         </div>
       </div>
     </div>,
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';     // Used here to navigate to another url (way to make modal window disappear) when user clicks on background modal screen.
 
 const StreamDelete=()=>{
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions} onDismiss={()=>history.push('/')}/>
       </div>
   )
 }
